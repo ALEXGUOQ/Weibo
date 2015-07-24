02d240bdbc9e3456c9928e61f99d8b8abbe94b0b
Eason <2314668686@qq.com>
2015-07-24 10:20:37 +0800

    项目初建

---
+++ WeiBo/WeiBo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiBo
+//
+//  Created by gryphon on 15/7/24.
+//  Copyright (c) 2015年 FalConnect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
